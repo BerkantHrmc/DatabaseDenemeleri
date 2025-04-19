4c18388fd39a09f97fb48562d98ce03c55a48dca
Berkant <berkanthurmaci2@gmail.com>
2025-04-19 20:32:52 +0300

    Proje dosyası ekle.

---
+++ DatabaseDenemeleri/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace DatabaseDenemeleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.kitapYeniDataSet = new DatabaseDenemeleri.KitapYeniDataSet();
+            this.sehirlerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.sehirlerTableAdapter = new DatabaseDenemeleri.KitapYeniDataSetTableAdapters.SehirlerTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.kitapYeniDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sehirlerBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.sehirlerBindingSource;
+            this.comboBox1.DisplayMember = "SehirAdi";
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(90, 110);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 30);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.ValueMember = "SehirAdi";
+            // 
+            // kitapYeniDataSet
+            // 
+            this.kitapYeniDataSet.DataSetName = "KitapYeniDataSet";
+            this.kitapYeniDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // sehirlerBindingSource
+            // 
+            this.sehirlerBindingSource.DataMember = "Sehirler";
+            this.sehirlerBindingSource.DataSource = this.kitapYeniDataSet;
+            // 
+            // sehirlerTableAdapter
+            // 
+            this.sehirlerTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 496);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.kitapYeniDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sehirlerBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private KitapYeniDataSet kitapYeniDataSet;
+        private System.Windows.Forms.BindingSource sehirlerBindingSource;
+        private KitapYeniDataSetTableAdapters.SehirlerTableAdapter sehirlerTableAdapter;
+    }
+}
+
---
+++ DatabaseDenemeleri/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaseDenemeleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: Bu kod satırı 'kitapYeniDataSet.Sehirler' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.sehirlerTableAdapter.Fill(this.kitapYeniDataSet.Sehirler);
+
+        }
+    }
+}
